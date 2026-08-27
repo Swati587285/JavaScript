ed9c35d97d7d713b3548593dcf0c99d8b7b20a18
Swati587285 <mfsi.swatik@gmail.com>
2026-08-27 11:29:42 +0530

    Initial JavaScript practice upload

---
+++ VariablesAndDataTypes/Console.js
@@ -0,0 +1,21 @@
+let Sname="Swati";
+let age=25;
+console.log(`Myself ${Sname} and My age is ${age}`);        //Myself Swati and My age is 25
+
+
+
+console.log("Hi this is Swati");    //  Hi this is Swati
+console.log(1 + "2" + "2");         //  122
+
+ a=0.1 + 0.2;
+ b=0.3;
+console.log(a==b);      //  false
+console.log(a);
+console.log(b);
+
+console.log(typeof BigInt);         //  function
+console.log(2 + 5);                 //  7
+console.log(10 - 5);                //  5
+console.log(2 * 5);                 //  10
+console.log(2 / 5);                 //  0.4
+console.log(25 % 5);                //  0
